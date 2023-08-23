2d4d19c27d58b4ca743631eb1ff20c983020d29e
ChoiSeaIn <epode4@naver.com>
2023-08-23 22:20:08 +0900

    게시물 수정 기능 추가

--- posts/views.py
+++ posts/views.py
@@ -30,15 +30,35 @@ def create(request):
     title = request.POST.get('title')
     content = request.POST.get('content')
 
-    # post = Post()s
+    # post = Post()
 
     post = Post(title=title, content=content)
     post.save()
 
     return redirect('posts:detail', id=post.id)
 
-def delete(requesets, id):
+def delete(request, id):
     post = Post.objects.get(id=id)
     post.delete()
 
     return redirect('posts:index')
+
+def edit(request, id):
+    post = Post.objects.get(id=id)
+
+    context = {
+        'post': post
+    }
+
+    return render(request, 'edit.html', context)
+
+def update(request, id):
+    title  = request.POST.get('title')
+    content = request.POST.get('content')
+
+    post = Post.objects.get(id=id)
+    post.title = title
+    post.content = content
+    post.save()
+
+    return redirect('posts:detail', id=post.id)
